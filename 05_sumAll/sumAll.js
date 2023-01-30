dd1bbbeaa0dd4453da162bcca24e2da556d09d32
SimonADW <simon-winter@outlook.com>
2023-01-30 22:01:39 +0100

    Pass all tests - fix isInteger snippet

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,11 @@
 const sumAll = function(num1, num2) {
     let sum = 0
-    if (num2 > num1) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR";       
+    } else if (!Number.isInteger(num1) || !Number.isInteger(num2)) {
+        return "ERROR"; 
+                       
+    } else if (num2 > num1) {
         for (let i = num2; i >= num1; i-- ) {
         sum += i;
         }
