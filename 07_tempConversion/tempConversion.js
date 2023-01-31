79f73ba3f8bfb9724e821c4a9b09ce98c6fc2994
SimonADW <simon-winter@outlook.com>
2023-01-31 19:18:47 +0100

    Completes function body toFahrenheit

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,19 @@
 const convertToCelsius = function(temp) {
-    let number = (temp - 32) * 0.5556
-    result = number.toFixed(1)
-    result = parseFloat(result)
+    let number = (temp - 32) * 0.5556;
+    result = number.toFixed(1);
+    result = parseFloat(result);
+    console.log(`The temperature is ${result} degrees celius`);
+    return result;
 };  
 
-const convertToFahrenheit = function() {
-};
+const convertToFahrenheit = function(temp) {
+  let number = (temp * 1.8) + 32;
+  result = number.toFixed(1);
+  result = parseFloat(result);
+  console.log(`The temperature is ${result} degrees Fahrenheit`);
+  return result;
+};  
+
 
 // Do not edit below this line
 module.exports = {
