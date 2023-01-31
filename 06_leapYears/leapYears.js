e2f42d5c6fae6d6a125ce5fc3860622c1ba7c3c6
SimonADW <simon-winter@outlook.com>
2023-01-31 18:47:25 +0100

    Complete if statement first try

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,11 @@
-const leapYears = function() {
+const leapYears = function(num) {
+    if ((Number.isInteger(num/4) && !Number.isInteger(num/100)) || Number.isInteger(num/400)) {
+        return true;
+        console.log("This is a leap year!");
+    } else {
+        return false;
+        console.log("Not a leap year")
+    } 
 
 };
 
